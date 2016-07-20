76cd940edec9589a2f6d559c60b41199941eee6b
Nguyen Cao Trung <trung.nguyen2@trginternational.com.vn>
2016-07-20 16:17:43 +0700

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,26 @@ var React = require('react');
 var About = () => {
 	return(
 		<div>
-			<h3>About Component</h3>
-			<p>My name is Nguyen Cao Trung</p>
+			<h1 className="text-center">About</h1>
+			<p>this is a weather application build on React. 
+			I have built this for the complete React Web App Developer
+			Course.
+			</p>
+
+			<p>
+				Here are some of the tool I used:
+			</p>
+
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This was
+					the JavaScript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to
+					search for weather data by city name.
+				</li>
+			</ul>
 		</div>
 		
 	)
